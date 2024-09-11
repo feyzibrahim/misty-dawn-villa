255c6154b45058b482d7eb0e51c7f92b6898dbd5
Feyz Ibrahim <46452412+feyzibrahim@users.noreply.github.com>
2024-09-11 12:09:04 +0530

    feat: gallery added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import AmenitiesIcons from "@/components/amenitiesIcons";
 export default function Home() {
 	return (
 		<div className="pt-5 px-5 lg:px-40">
-			<main className="h-screen pt-24 pb-16">
+			<main className="lg:h-screen pt-24 pb-16">
 				<div
-					className={`bg-no-repeat  rounded-3xl overflow-clip flex items-center justify-center flex-col`}
+					className={`bg-no-repeat py-10 rounded-3xl relative overflow-clip flex items-center justify-center flex-col`}
 					style={{
 						backgroundImage: `url(${bg.src})`,
 						width: "100%",
@@ -17,10 +17,11 @@ export default function Home() {
 						backgroundPosition: "50% 50%",
 					}}
 				>
-					<h1 className="text-white text-4xl text-center text-shadow lg:w-2/3">
+					<div className="bg-black h-full w-full absolute -z-0 opacity-50"></div>
+					<h1 className="text-white text-xl lg:text-4xl z-0 text-center text-shadow lg:w-2/3">
 						Enjoy the Luxurious Stay at the Resorts of MistyDawn Villa
 					</h1>
-					<p className="text-center text-white text-shadow">
+					<p className="text-center text-xs lg:text-sm z-0 text-white text-shadow">
 						Experience opulent luxury and exceptional service at the exquisite
 						MistyDawn Villa
 					</p>
@@ -73,21 +74,18 @@ export default function Home() {
 						src="/img/cropped/34.jpg"
 						width={3024}
 						height={4032}
-						className="rounded-md"
 					/>
 					<Image
 						alt="rooms 1"
 						src="/img/cropped/32.jpg"
 						width={3024}
 						height={4032}
-						className="rounded-md"
 					/>
 					<Image
 						alt="rooms 2"
 						src="/img/cropped/33.jpg"
 						width={3024}
 						height={4032}
-						className="rounded-md"
 					/>
 				</div>
 			</div>
@@ -107,6 +105,99 @@ export default function Home() {
 					</p>
 				</div>
 				<AmenitiesIcons />
+			</div>
+
+			<div className="min-h-screen space-y-5 py-10">
+				<p className="text-primary font-bold uppercase">Gallery</p>
+				<h1 className={`${oldStandardTT.className} text-5xl font-bold`}>
+					A Pictorial Escape
+				</h1>
+				<div className="w-32 h-1 bg-primary rounded-full flex-shrink-0 lg:mt-5"></div>
+				<div className="columns-2 gap-4 sm:columns-2 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-4">
+					<Image
+						alt="rooms 3"
+						src="/img/36.jpg"
+						width={3024}
+						height={4032}
+						className="rounded-md"
+					/>
+					<Image
+						alt="rooms 3"
+						src="/img/cropped/5.jpg"
+						width={1868}
+						height={2936}
+						className="rounded-md"
+					/>
+					<Image
+						alt="rooms 3"
+						src="/img/2.jpg"
+						width={3024}
+						height={4032}
+						className="rounded-md"
+					/>
+
+					<Image
+						alt="rooms 3"
+						src="/img/37.jpg"
+						width={3024}
+						height={4032}
+						className="rounded-md"
+					/>
+
+					<Image
+						alt="rooms 3"
+						src="/img/3.jpg"
+						width={3000}
+						height={4000}
+						className="rounded-md"
+					/>
+
+					<Image
+						alt="rooms 3"
+						src="/img/6.jpg"
+						width={3024}
+						height={4032}
+						className="rounded-md"
+					/>
+
+					<Image
+						alt="rooms 3"
+						src="/img/17.jpg"
+						width={3024}
+						height={4032}
+						className="rounded-md"
+					/>
+
+					<Image
+						alt="rooms 3"
+						src="/img/30.jpg"
+						width={3024}
+						height={4032}
+						className="rounded-md"
+					/>
+					<Image
+						alt="rooms 3"
+						src="/img/26.jpg"
+						width={3024}
+						height={4032}
+						className="rounded-md"
+					/>
+
+					<Image
+						alt="rooms 3"
+						src="/img/21.jpg"
+						width={3024}
+						height={4032}
+						className="rounded-md"
+					/>
+					<Image
+						alt="rooms 3"
+						src="/img/38.jpg"
+						width={2268}
+						height={4032}
+						className="rounded-md"
+					/>
+				</div>
 			</div>
 
 			{/* <footer
